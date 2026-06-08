9a7fa8615c7bd2e2bf4d2e8e9fe3456fec43439b
Omezi42 <Omezi4242@gmail.com>
2026-06-08 17:54:18 +0900

    Update databaseURL to match the correct Firebase region

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA0n2qx8M2qHiwvepKjqTm_ECcZ3E1AnW8",
   authDomain: "anokorolinq.firebaseapp.com",
   projectId: "anokorolinq",
-  databaseURL: "https://anokorolinq-default-rtdb.firebaseio.com",
+  databaseURL: "https://anokorolinq-default-rtdb.asia-southeast1.firebasedatabase.app",
   storageBucket: "anokorolinq.firebasestorage.app",
   messagingSenderId: "193022894612",
   appId: "1:193022894612:web:0f7487d7ac2b245f347d01",
